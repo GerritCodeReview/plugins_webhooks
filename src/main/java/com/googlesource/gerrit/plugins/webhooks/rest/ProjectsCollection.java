b620659883781318cf3745582df6291acb2b9dad
David Ostrovsky <david@ostrovsky.org>
2019-05-26 14:06:34 +0200

    Rename reviewdb package to entities
    Change-Id: I9b8995d179701a6bdf00390d30f5692e28fc45b1

--- src/main/java/com/googlesource/gerrit/plugins/webhooks/rest/ProjectsCollection.java
+++ src/main/java/com/googlesource/gerrit/plugins/webhooks/rest/ProjectsCollection.java
@@ -14,13 +14,13 @@
 
 package com.googlesource.gerrit.plugins.webhooks.rest;
 
+import com.google.gerrit.entities.Project;
 import com.google.gerrit.extensions.registration.DynamicMap;
 import com.google.gerrit.extensions.restapi.ChildCollection;
 import com.google.gerrit.extensions.restapi.IdString;
 import com.google.gerrit.extensions.restapi.NotImplementedException;
 import com.google.gerrit.extensions.restapi.ResourceNotFoundException;
 import com.google.gerrit.extensions.restapi.RestView;
-import com.google.gerrit.reviewdb.client.Project;
 import com.google.gerrit.server.config.ConfigResource;
 import com.google.gerrit.server.project.ProjectCache;
 import com.google.inject.Inject;
