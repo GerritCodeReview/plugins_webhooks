19c8981bacd9e22a53dfd7c200330c93819ccdf3
David Ostrovsky <david@ostrovsky.org>
2026-06-12 10:28:14 +0200

    Use org.apache.http.HttpStatus constants for status code checks
    Replace the static imports of
    javax.servlet.http.HttpServletResponse.SC_* with the equivalent
    constants from org.apache.http.HttpStatus, which is already a
    transitive dependency of every Gerrit plugin via the plugin API
    (plugins/BUILD exports //lib/httpcomponents:httpclient). Affects
    HttpResponseHandler (main) plus the GetRemoteIT and UpdateRemoteIT
    acceptance tests.
    
    HttpResponseHandler is an Apache HttpClient ResponseHandler and
    already imports several other org.apache.http.* types, so using
    org.apache.http.HttpStatus here is the consistent choice. After
    this CL the affected files compile cleanly against any Gerrit
    build (javax.servlet or jakarta.servlet) without flavour-specific
    source.
    
    No behaviour change - the integer values are identical.
    
    Change-Id: I4f654e9215478ce7348038df889154b8ec61a610

--- src/main/java/com/googlesource/gerrit/plugins/webhooks/HttpResponseHandler.java
+++ src/main/java/com/googlesource/gerrit/plugins/webhooks/HttpResponseHandler.java
@@ -14,10 +14,10 @@
 
 package com.googlesource.gerrit.plugins.webhooks;
 
-import static javax.servlet.http.HttpServletResponse.SC_CREATED;
-import static javax.servlet.http.HttpServletResponse.SC_NO_CONTENT;
-import static javax.servlet.http.HttpServletResponse.SC_OK;
-import static javax.servlet.http.HttpServletResponse.SC_ACCEPTED;
+import static org.apache.http.HttpStatus.SC_ACCEPTED;
+import static org.apache.http.HttpStatus.SC_CREATED;
+import static org.apache.http.HttpStatus.SC_NO_CONTENT;
+import static org.apache.http.HttpStatus.SC_OK;
 
 import com.google.common.flogger.FluentLogger;
 import com.googlesource.gerrit.plugins.webhooks.HttpResponseHandler.HttpResult;
